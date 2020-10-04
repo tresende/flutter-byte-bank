cf213e79e0aa459ceca3cc8faff7f7a8651fd8a0
Thiago Resende <thiago.gcresende@gmail.com>
2020-10-04 20:09:26 -0300

    Adicoonando cards

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -2,6 +2,22 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
       home: Scaffold(
+        body: Column(
+          children: [
+            Card(
+              child: ListTile(
+                  leading: Icon(Icons.monetization_on),
+                  title: Text("100.0"),
+                  subtitle: Text("1000")),
+            ),
+            Card(
+              child: ListTile(
+                  leading: Icon(Icons.monetization_on),
+                  title: Text("100.0"),
+                  subtitle: Text("1000")),
+            ),
+          ],
+        ),
         appBar: AppBar(
           title: const Text('Transferências'),
         ),
